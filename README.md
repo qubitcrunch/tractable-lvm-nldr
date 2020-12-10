41ce9abc864de5de2f018320f30a92e95ac04970
Tejash Desai <tdesai@ntent.com>
2020-12-09 22:28:21 -0800

    Fill main.py partially

--- README.md
+++ README.md
@@ -5,9 +5,12 @@ Pre-print available here - https://cseweb.ucsd.edu/~saul/pnas20/preprint.pdf
 
 # Project Structure
 dist_helpers.py -> Houses some helpers that calculate a variety of distance function. The choice of distance function is
-                    ultimately reflected in knn_and_graph_funcs.compute_dis
+                    ultimately reflected in knn_and_graph_funcs.compute_dis           
+                    
 em_helpers.py -> Houses the init_graph function that returns an initial value of \mu0 as well as the EM-learning 
                     algorithm
+                    
 knn_and_graph_funcs.py -> Houses various KNN functions, functions to calculate similarity and dis-similarity pairs 
                             given a tree
+                            
 main.py -> the main file that runs the algorithm step-by-step
--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-pass
+import argparse
+import numpy as np
+from knn_and_graph_funcs import *
+from em_helpers import *
+
+
+def load_data(file_path: str) -> np.ndarray:
+    """
+
+    Args:
+        file_path:
+
+    Returns:
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Run tractable LVM for Nonlinear-dimensionality reduction')
+    parser.add_argument('data-file', type=str, required=True, help='absolute path of the file containing data')
+    parser.add_argument('k', type=int, required=True, help='number of nearest neighbors')
+    parser.add_argument('s', type=int, required=True, help='steps along nearest neighbor graph')
+    parser.add_argument('l', type=int, required=True, help='levels of coarse-graining')
+    parser.add_argument('d', type=int, required=True, help='dimensionality of embedding')
+    parser.add_argument('max-iter-em', type=int, default=400)
+    parser.add_argument('momentum', type=float, default=0.9)
+    parser.add_argument('print-cost', type=int, default=10)
+    parser.add_argument('cmg-pcg-tol', type=float, default=1e-6)
+    parser.add_argument('cmg-pcg-max-iter', type=int, default=50)
+    args = parser.parse_args()
+
+    X = load_data(args.data_file)
+    E = compute_neighborhood_graph(X, args.k, args.s)
+    # coarse graining
+    cg_tree = coarse_grain(X, args.l)
+    # similar and dissimilar examples
+    sim_pairs = compute_sim(cg_tree, E)
+    dis_pairs = compute_dis(cg_tree, sim_pairs)
+    # initiliaze
+    mu0 = init_from_graph_laplacian(sim_pairs, d)
+    # learn
+    (mu, sigma_sq) = learn(mu0, sim_pairs, dis_pairs, args)
+
+
